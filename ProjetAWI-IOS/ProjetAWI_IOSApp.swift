4e818537561ae1480bf908a0077d675cfe01c9d4
Xavier-Corbier <xavier.corbier@etu.umontpellier.fr>
2022-02-17 13:47:43 +0100

    Initial Commit

---
+++ ProjetAWI-IOS/ProjetAWI_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetAWI_IOSApp.swift
+//  ProjetAWI-IOS
+//
+//  Created by m1 on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjetAWI_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
